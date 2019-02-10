5ba5f818099d7a69003b5be951f6ff68148b8636
MH.Movasaghi <mmghho77@gmail.com>
2019-02-10 16:00:37 +0330

    Add Http Ref (Cont)

---
+++ noskhe_Admin/noskhe_Admin/Controller/Medicion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace noskhe_Admin.Controller
+{
+    public class Medicine
+    {
+        public int MedicineId { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string ProductPictureUrl { get; set; }
+        public DateTime ProductPictureUploadDate { get; set; }
+        public bool Type { get; set; }
+        
+    }
+}
